d18e1c7d8016a3630ec5121c0525380db99d15f1
Bryan Brunetti <bryan.brunetti@gmail.com>
2020-03-19 11:18:37 -0400

    tidying up the background, and making the window draggable

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,7 +1,6 @@
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
@@ -9,29 +8,26 @@ import java.io.IOException;
 
 public class Controller {
     @FXML
-    public ChoiceBox<String> deviceListBox;
-    public ObservableList<String> deviceList = FXCollections.observableArrayList("Searching ...");
+    private ChoiceBox deviceListBox;
+    @FXML
+    private ImageView remoteControl;
 
+//    public ObservableList<String> deviceList = FXCollections.observableArrayList("Searching ...");
 
-    public Controller() {
-        RokuAPI.discover(this);
-    }
     public void homeButton() {
-        System.out.println("Home Button");
+        System.out.println("Home Button Pressed");
+        System.out.println(deviceListBox);
+//        deviceListBox.getItems().add("Rokue!");
     }
 
     public void backButton() {
         System.out.println("Back Button");
     }
 
-    public void beginSearching() {
-        deviceListBox.setItems(deviceList);
-    }
-
     public void newDeviceFound(String deviceLocation) throws IOException, InterruptedException {
         String deviceInfo = RokuAPI.getDeviceInfo(deviceLocation);
         SimpleXMLParser parser = new SimpleXMLParser(deviceInfo);
         String deviceName = parser.findByTagName("user-device-name");
-        deviceList.add(deviceName);
+//      deviceList.add(deviceName);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,34 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    private double windowXOffset = 0;
+    private double windowYOffset = 0;
 
     @Override
     public void start(Stage stage) throws IOException {
         Controller controller = new Controller();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = loader.load();
         loader.setController(controller);
 
-        RokuAPI.discover(controller);
-        Parent root = loader.load();
+        root.setStyle("-fx-background-color: rgba(0, 0, 0, 0.0);");
+
         stage.setTitle("JokuRemote");
-        stage.setScene(new Scene(root, 250, 800));
+        Scene scene = new Scene(root, 250, 800);
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        setWindowDraggable(root, stage);
         stage.show();
     }
 
@@ -25,8 +36,25 @@ public class Main extends Application {
         launch();
     }
 
-    public static void onShutdown() {
-//        RokuAPI.stopDiscovery();
+    public void stop() {
+        System.out.println("Stopping JokuRemote");
+    }
+
+    public void setWindowDraggable(Parent root, Stage stage) {
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                windowXOffset = event.getSceneX();
+                windowYOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - windowXOffset);
+                stage.setY(event.getScreenY() - windowYOffset);
+            }
+        });
     }
 
 }
