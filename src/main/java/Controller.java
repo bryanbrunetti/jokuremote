cf3819ba02f0cce91e566a659c93831e720a943f
Bryan Brunetti <bryan.brunetti@gmail.com>
2020-03-20 08:34:10 -0400

    channels are linked

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,7 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,12 +19,33 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
     public HashMap<String,String> deviceListMap = new HashMap<>();
     public ObservableList<String> devices = FXCollections.observableArrayList();
-    @FXML private ComboBox<String> deviceListBox;
-    @FXML private ImageView remoteControl;
 
+    @FXML private ComboBox<String> deviceListBox;
+    @FXML private ListView<ImageView> channelList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        deviceListBox.setItems(devices);
+        deviceListBox.setValue("Select a Roku ...");
+        discoverDevices();
+    }
+
+    public void loadChannels() throws IOException, InterruptedException {
+        String result = RokuAPI.apiCall(getSelectedDeviceAddress() + "query/apps");
+
+        SimpleXMLParser parser = new SimpleXMLParser(result);
+        for(Object appId : parser.parseTagAttribute("id")) {
+            Image image = new Image(getSelectedDeviceAddress() + "query/icon/" + appId);
+            ImageView imageView = new ImageView(image);
+            imageView.setId((String) appId);
+            imageView.setPreserveRatio(true);
+
+            imageView.setFitWidth(152);
+            channelList.getItems().add(imageView);
+        }
+    }
     @FXML
     public void discoverDevices() {
-        System.out.println("discover can see?: "+ deviceListBox + remoteControl);
         RokuAPI.setDiscoveryListener(this);
     }
 
@@ -45,15 +69,21 @@ public class Controller implements Initializable {
         System.out.println("APICALL: " + selectedDevice + apiCall);
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        System.out.println("initialize: " + deviceListBox);
-        deviceListBox.setItems(devices);
-        deviceListBox.setValue("Select a Roku ...");
-        discoverDevices();
+    public String getSelectedDeviceAddress() {
+        return deviceListMap.get(deviceListBox.getSelectionModel().getSelectedItem());
     }
 
-    public void changeDevices(ActionEvent actionEvent) {
-        System.out.println("Device Change: " + actionEvent);
+
+    public void changeDevices(ActionEvent actionEvent) throws IOException, InterruptedException {
+//        ComboBox cb = (ComboBox) actionEvent.getSource();
+//        System.out.println(cb.getSelectionModel().getSelectedItem());
+//        System.out.println("Device Change: " + actionEvent);
+//        System.out.println(this.getSelectedDeviceAddress());
+        loadChannels();
+    }
+
+    public void changeChannel(MouseEvent mouseEvent) {
+        ImageView channel = channelList.getSelectionModel().getSelectedItem();
+        System.out.println("API CALL: " + getSelectedDeviceAddress() + "launch/" + channel.getId());
     }
 }
--- src/main/java/SimpleXMLParser.java
+++ src/main/java/SimpleXMLParser.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,13 +10,23 @@ public class SimpleXMLParser {
     }
 
     public String findByTagName(String tagName) {
-        Pattern regEx = Pattern.compile("<"+tagName+">(.*)</"+tagName+">");
+        Pattern regEx = Pattern.compile("<" + tagName + ">(.*)</" + tagName + ">");
         Matcher matcher = regEx.matcher(xml);
 
-        if(matcher.find()) {
+        if (matcher.find()) {
             return matcher.group(1);
         }
         return "";
+    }
+
+    public ArrayList parseTagAttribute(String attr) {
+        Pattern regEx = Pattern.compile(attr + "=\"(.*?)\"");
+        Matcher matcher = regEx.matcher(xml);
+        var attributes = new ArrayList();
+        while (matcher.find()) {
+            attributes.add(matcher.group(1));
+        }
+        return attributes;
     }
 }
 
